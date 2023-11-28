4a218dff90a558546dd745ebfb7842dd65be418e
tuxuan22 <haxuantu302@gmail.com>
2023-11-28 11:49:00 +0700

    fix conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import Head from "next/head";
-import Link from "next/link";
-
-export default function Page({ repo }) {
-=======
 import Head from 'next/head';
-import Link from 'next/link';
+// import Link from 'next/link';
 import {useRouter} from 'next/router';
 import {getCookie, hasCookie, deleteCookie} from 'cookies-next';
 import Nav from '@/component/nav';
@@ -18,20 +12,11 @@ export default function Page(props) {
         router.reload();
     };
 
->>>>>>> 0e58d1ae63ea0d1247746cbbb02ad23eecb23fcb
     return (
         <>
             <Head>
                 <title>Home</title>
             </Head>
-<<<<<<< HEAD
-            <h1>Home</h1>
-            <Link href={"/login"}>Login</Link>
-            <Link href={"/register"}>Register</Link>
-        </>
-    );
-}
-=======
             <Nav hascookie={props.hascookie} />
             <div>
                 <br />
@@ -174,4 +159,3 @@ export const getServerSideProps = ({req, res}) => {
     const hascookie = hasCookie('id_nguoidung', {req, res});
     return {props: {cookie, hascookie}};
 };
->>>>>>> 0e58d1ae63ea0d1247746cbbb02ad23eecb23fcb
